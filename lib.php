adfe6d359a312a947006a9d2ae3114980caf719f
Abu Ashraf Masnun <masnun@gmail.com>
2011-08-31 22:02:14 +0600

    Changed Timeout

--- lib.php
+++ lib.php
@@ -16,7 +16,7 @@ function curl_post($url, array $post = NULL, array $options = array())
         CURLOPT_FRESH_CONNECT => 1,
         CURLOPT_RETURNTRANSFER => 1,
         CURLOPT_FORBID_REUSE => 1,
-        CURLOPT_TIMEOUT => 4,
+        CURLOPT_TIMEOUT => 10,
         CURLOPT_POSTFIELDS => http_build_query($post)
     );
 
